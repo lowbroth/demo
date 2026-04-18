61bfb594a7cfe6f188d01a77574462d2eef8e328
liuxiejun <1013125925@qq.com>
2026-04-18 13:29:03 +0800

    初次使用

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
 	engine := gin.Default()
+	viper.New()
+	viper.SetConfigName("configs/demo.yml")
+	serviceConfig := &ServerConfig{}
+	viper.Unmarshal(serviceConfig)
+
 	engine.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "success", "version": 1.0})
+		c.JSON(200, gin.H{"message": "success", "version": 1.0, "name": serviceConfig.ServiceName})
 	})
 	engine.Run(":8080")
+}
+
+type MysqlConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+type ServerConfig struct {
+	ServiceName string      `mapstructure:"name"`
+	MysqlInfo   MysqlConfig `mapstructure:"mysql"`
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
